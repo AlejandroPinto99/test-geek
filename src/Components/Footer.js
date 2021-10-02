919518a551836fbbe7224d568620f914d99e4f68
AlejandroPinto99 <00156618@uca.edu.sv>
2021-10-02 00:44:05 -0600

    opacity

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
         <div className="flex flex-row justify-center mb-3">
-            <p className="font-bold text-blue-500 opacity-50"> Test creé par Alejandro Pinto</p>
+            <p className="font-bold text-blue-500 opacity-60"> Test creé par Alejandro Pinto</p>
         </div>
     )
 }
